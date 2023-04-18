b285454ed44ceca11bd5ef7b2ae8e3add053a0b3
Gourav Singh Chauhan <gouravsinghc2003@gmail.com>
2023-04-19 00:58:59 +0530

    Added Intent launchBrowser to search directly when Learn More Button is clicked and the switch 'Use OpenAI' is off.

--- app/src/main/java/com/project/myapplication/MainActivity.java
+++ app/src/main/java/com/project/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.project.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -13,40 +15,33 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
-import com.google.gson.Gson;
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
-import com.theokanning.openai.*;
-import com.theokanning.openai.completion.CompletionRequest;
-
-import okio.AsyncTimeout;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button mButton;
-    private static Button mButton2;
+
     private TextView mTextView;
     private TextView mTextView1;
     private TextView mTextView2;
     private TextView mTextView3;
     private TextView mTextView4;
+    private static String activity;
+    private SwitchCompat mSwitch1;
     private static TextView poph;
     private static TextView pop;
-    private static String activity;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,47 +51,44 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        final Vibrator vib = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
-
-        mButton = findViewById(R.id.button);
-        mButton2 = findViewById(R.id.button2);
+        final Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        Button mButton = findViewById(R.id.button);
+        Button mButton2 = findViewById(R.id.button2);
         mTextView = findViewById(R.id.textView);
         mTextView1 = findViewById(R.id.textView4);
         mTextView2 = findViewById(R.id.textView6);
         mTextView3 = findViewById(R.id.textView8);
         mTextView4 = findViewById(R.id.textView10);
-
-
+        mSwitch1 = findViewById(R.id.switch1);
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.popup, null);
         int width = LinearLayout.LayoutParams.MATCH_PARENT;
         int height = LinearLayout.LayoutParams.MATCH_PARENT;
         poph = popupView.findViewById(R.id.textView12);
         pop = popupView.findViewById(R.id.popup_text);
-        //mProgressBar = popupView.findViewById(R.id.progressBar);
         boolean focusable = true;
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
 
-
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new BoredApiTask().execute();
-                vib.vibrate(80);
-            }
+        mButton.setOnClickListener(view -> {
+            new BoredApiTask().execute();
+            vib.vibrate(80);
         });
-        mButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
+        mButton2.setOnClickListener(view -> {
+            boolean switchState = mSwitch1.isChecked();
+            if(switchState){
                 popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
                 new howto().execute();
-
             }
-            });
+            else {
+                Uri uriUrl = Uri.parse("https://www.google.com/search?q=" + "How to " + activity);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
     }
 
     public class BoredApiTask extends AsyncTask<Void, Void, String> {
+
         @Override
         protected String doInBackground(Void... voids) {
             try {
@@ -105,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 connection.connect();
                 InputStream inputStream = connection.getInputStream();
                 Scanner scanner = new Scanner(inputStream);
-                scanner.useDelimiter("\\A");
+                scanner.useDelimiter(getResources().getString(R.string.delimi));
                 String responseBody = "";
                 if (scanner.hasNext()) {
                     responseBody = scanner.next();
@@ -115,33 +107,28 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("MainActivity", "Error: " + e.getMessage());
             }
             return null;
-
         }
 
         @Override
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-        try {
-            JSONObject json = new JSONObject(result);
-            activity = json.getString("activity");
-            String typeo = json.getString("type");
-            String parti = json.getString("participants");
-            String price = json.getString("price");
-            String acc = json.getString("accessibility");
-            mTextView.setText(activity);
-            mTextView1.setText(typeo);
-            mTextView2.setText(parti);
-            mTextView3.setText(price);
-            mTextView4.setText(acc);
-
-
-        } catch (JSONException e) {
-            Log.e("MainActivity", "Error parsing JSON: " + e.getMessage());
-            mTextView.setText("Error");
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            try {
+                JSONObject json = new JSONObject(result);
+                activity = json.getString("activity");
+                String typeo = json.getString("type");
+                String parti = json.getString("participants");
+                String price = json.getString("price");
+                String acc = json.getString("accessibility");
+                mTextView.setText(activity);
+                mTextView1.setText(typeo);
+                mTextView2.setText(parti);
+                mTextView3.setText(price);
+                mTextView4.setText(acc);
+            } catch (JSONException e) {
+                Log.e("MainActivity", "Error parsing JSON: " + e.getMessage());
+                mTextView.setText(R.string.error);
+            }
         }
-
-        }
-
     }
 
     private static class howto extends AsyncTask<String, Void, String> {
@@ -172,5 +159,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-
 
